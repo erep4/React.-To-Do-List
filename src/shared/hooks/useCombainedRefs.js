62ab9a7aa4c584e8be7b349945aea36a4661c5b1
Oleg Ponomarjov <erep4@inbox.lv>
2026-06-15 12:48:00 +0300

    to do

---
+++ src/shared/hooks/useCombainedRefs.js
@@ -0,0 +1,17 @@
+const useCombainedRefs = (...refs) => {
+  return (node) => {
+    refs.forEach((ref) => {
+      if (!ref) {
+        return;
+      }
+
+      if (typeof ref === 'function') {
+        ref(node)
+      } else {
+        ref.current = node
+      }
+    });
+  };
+};
+
+export default useCombainedRefs;
