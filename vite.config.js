91f8eb7f48d670fee87d62ba26279a83a0f42cf6
Oleg Ponomarjov <73836247+erep4@users.noreply.github.com>
2026-07-08 11:50:22 +0300

    Update vite.config.js
    little update

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const isProd = mode === 'production'
 
   return {
-    base: isProd ? '/todo-react/' : '/',
+    base: isProd ? '/React.-To-Do-List/' : '/',
     plugins: [react()],
     resolve: {
       alias: {
